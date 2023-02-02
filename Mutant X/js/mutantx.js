82deef85b803b92985e288b93ff1e04735cf7585
David Betancur <juan.david.betancur@outlook.com>
2023-02-02 11:00:35 -0500

    Update Javascript functions
    Update Javascript functions per attack and count per mutant

--- Mutant X/js/mutantx.js
+++ Mutant X/js/mutantx.js
@@ -1,6 +1,20 @@
+let playerAttack
+let enemyAttack
+
+
+
 function startGame() {
     let buttonSelectMutant = document.getElementById('button_selectmutant')
     buttonSelectMutant.addEventListener('click', selectMascotPlayer);
+
+    let buttonFire=document.getElementById('button_fire')
+    buttonFire.addEventListener('click', fireAttack);
+    
+    let buttonWater= document.getElementById('button_water')
+    buttonWater.addEventListener('click', waterAttack);
+
+    let buttonForest = document.getElementById('button_forest');
+    buttonForest.addEventListener('click',forestAttack);
 }
 
 function selectMascotPlayer() {
@@ -44,6 +58,70 @@ function SelectMascotEnemy() {
     }
 
 }
+
+function fireAttack() {
+    playerAttack = 'Fuego Mortal'
+    enemyRandomAttack()
+}
+function waterAttack() {
+    playerAttack = 'Cañón de Agua'
+    enemyRandomAttack()
+}
+
+function forestAttack() {
+    playerAttack = 'Bosque Vivo'
+    enemyRandomAttack()
+}
+
+function enemyRandomAttack() {
+    let randomAttack = aleatorio(1,3)
+
+    if (randomAttack == 1) {
+        enemyAttack= 'Fuego Mortal'
+    }
+    else if (randomAttack == 2) {
+        enemyAttack='Cañón Agua'
+    }
+    else if (randomAttack == 3) {
+        enemyAttack='Bosque Vivo'
+    }
+
+    combat();
+}
+
+function createMessage(resultAttack) {
+    let sectionMessage = document.getElementById('section_messages')
+    let attackParagraph = document.createElement('p')
+    attackParagraph.innerHTML = 'Tu mutante atacó con ' + playerAttack + ' y tu enemigo atacó con ' + enemyAttack +', ' + resultAttack
+    sectionMessage.appendChild(attackParagraph)
+    
+}
+
+function combat() {
+    
+    if (enemyAttack == playerAttack) {
+        createMessage('Empate')
+    }
+    else if (enemyAttack == 'Fuego Mortal' && playerAttack == 'Cañón de Agua') {
+        createMessage('Ganaste el movimiento')
+    }
+    else if (enemyAttack == 'Fuego Mortal' && playerAttack == 'Bosque Vivo') {
+        createMessage('Perdiste el movimiento')
+    }
+    else if (enemyAttack == 'Cañón de Agua' && playerAttack == 'Bosque Vivo') {
+        createMessage('Ganaste el movimiento')
+    }
+    else if (enemyAttack == 'Cañón de Agua' && playerAttack == 'Fuego Mortal') {
+        createMessage('Perdiste el movimiento')
+    }
+    else if (enemyAttack == 'Bosque Vivo' && playerAttack == 'Cañón de Agua') {
+        createMessage('Perdiste el movimiento')
+    }
+    else if (enemyAttack == 'Bosque Vivo' && playerAttack == 'Fuego Mortal') {
+        createMessage('Ganaste el movimiento')
+    }
+}
+
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
